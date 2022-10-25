6d901fe9040856b8602ec9513f3b79cc06ab5638
anupriyalathey <anupriyalathey@gmail.com>
2022-10-26 00:58:45 +0530

    adding swap.sol functionality

--- front_end/src/pages/Home.js
+++ front_end/src/pages/Home.js
@@ -28,7 +28,7 @@ function Home() {
         {data.map((data) => (
           <div key={data.title}>
             <p>{data.title}</p>
-            <img src={data.image} style={{ width: "100%" }}></img>
+            <img src={data.image} style={{ width: "100%" }} alt=""></img>
           </div>
         ))}
       </section>
